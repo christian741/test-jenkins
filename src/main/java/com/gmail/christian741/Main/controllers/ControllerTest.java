47e6a92468995522f9163faa8363f5632709c8e2
christian741 <christian.xd.417@gmail.com>
2021-06-08 23:12:53 -0500

    controller

--- src/main/java/com/gmail/christian741/Main/controllers/ControllerTest.java
+++ src/main/java/com/gmail/christian741/Main/controllers/ControllerTest.java
@@ -4,14 +4,16 @@ package com.gmail.christian741.Main.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
 @RestController
+@RequestMapping("/user")
 public class ControllerTest {
 
-	@GetMapping
+	@GetMapping("/getAlls")
 	public ResponseEntity<?> getAlls(
 			){
 		
